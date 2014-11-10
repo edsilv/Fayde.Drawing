344e704dca04580d00386a80f693080bb42f60f4
edsilv <e.silverton@gmail.com>
2014-11-10 23:15:45 +0000

    build

--- Fayde.Drawing.js
+++ Fayde.Drawing.js
@@ -1,7 +1,7 @@
 var Fayde;
 (function (Fayde) {
     (function (Drawing) {
-        Drawing.Version = '0.4.0';
+        Drawing.Version = '0.2.2';
     })(Fayde.Drawing || (Fayde.Drawing = {}));
     var Drawing = Fayde.Drawing;
 })(Fayde || (Fayde = {}));
